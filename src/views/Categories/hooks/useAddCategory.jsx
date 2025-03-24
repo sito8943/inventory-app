b9e703086c46aacd7c5e3fccc250961716cf4e0c
Sito <sito8943@gmail.com>
2025-03-24 08:47:22 -0400

    fixed missing react query keys and do reset

--- src/views/Categories/hooks/useAddCategory.jsx
+++ src/views/Categories/hooks/useAddCategory.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useMutation } from "@tanstack/react-query";
 import { useForm } from "react-hook-form";
 import { useTranslation } from "react-i18next";
@@ -6,12 +6,15 @@ import { useTranslation } from "react-i18next";
 // providers
 import { queryClient, useManager } from "../../../providers/ManagerProvider";
 
+// utils
+import { ReactQueryKeys } from "../../../utils/queryKey";
+
 function useAddCategory() {
   const { t } = useTranslation();
 
   const manager = useManager();
 
-  const { control, handleSubmit } = useForm();
+  const { control, handleSubmit, reset } = useForm();
 
   const [open, setOpen] = useState();
 
@@ -27,21 +30,22 @@ function useAddCategory() {
     },
     onSuccess: () => {
       queryClient.invalidateQueries([ReactQueryKeys.Categories]);
+      reset();
       //TODO THROW NOTIFICATION HERE
     },
   });
 
+  useEffect(() => {
+    if (!open) reset();
+  }, [open]);
+
   return {
     onClick,
-    formProps: {
-      handleSubmit: handleSubmit((d) => addFn.mutate(d)),
-      control,
-    },
-    dialogProps: {
-      title: t("_pages:categories.forms.add"),
-      open,
-      handleClose,
-    },
+    title: t("_pages:categories.forms.add"),
+    open,
+    control,
+    handleSubmit: handleSubmit((data) => addFn.mutate(data)),
+    handleClose,
   };
 }
 
