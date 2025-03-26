1035d7109a53e883929bc56d0071c21f2e76f281
Sito <sito8943@gmail.com>
2025-03-25 22:37:09 -0400

    color preview

--- src/components/Form/ColorInput.jsx
+++ src/components/Form/ColorInput.jsx
@@ -2,11 +2,20 @@ import { faEyeDropper } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useState, forwardRef } from "react";
 
+// @emotion/css
+import { css } from "@emotion/css";
+
 // components
 import TextInput from "./TextInput";
 
 const ColorInput = forwardRef(function (props, ref) {
   const [showDialog, setShowDialog] = useState();
+
+  const { value } = props;
+
+  const styles = css({
+    backgroundColor: value,
+  });
 
   return (
     <TextInput
@@ -20,6 +29,11 @@ const ColorInput = forwardRef(function (props, ref) {
         >
           <FontAwesomeIcon icon={faEyeDropper} className="text-white text-sm" />
         </button>
+      }
+      endAdornment={
+        <span
+          className={`rounded-full absolute right-1 top-[50%] -translate-y-[50%] w-5 h-5 ${styles}`}
+        ></span>
       }
     />
   );
