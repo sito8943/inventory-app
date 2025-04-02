a6f6742b38e615ac07b9a3416d24419c386476a2
Sito <sito8943@gmail.com>
2025-04-02 10:07:39 -0400

    fixed type name

--- src/lib/Notification.js
+++ src/lib/Notification.js
@@ -11,7 +11,7 @@ export class Notification {
   }
 }
 
-export class NotificationContext {
+export class NotificationContextType {
   notification = [];
   removeNotification = () => {};
   showErrorNotification = () => {};
