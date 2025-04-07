4815793d176a7188eeefb966ac7c4e44daf91d7b
Sito <sito8943@gmail.com>
2025-04-07 08:28:33 -0400

    added movement default types

--- src/db/types/products.js
+++ src/db/types/products.js
@@ -3,6 +3,17 @@ export const MovementTypes = {
   OUT: 2,
 };
 
+export const types = [
+  {
+    id: 1,
+    label: "in",
+  },
+  {
+    id: 2,
+    label: "out",
+  },
+];
+
 export class MovementDto {
   id = 0;
   movement = 0;
