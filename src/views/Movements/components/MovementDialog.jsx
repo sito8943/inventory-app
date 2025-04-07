cb5dd42632fbcf38849648b0918a4ab1d8e13517
Sito <sito8943@gmail.com>
2025-04-07 08:28:57 -0400

    added is loading to dialogs

--- src/views/Movements/components/MovementDialog.jsx
+++ src/views/Movements/components/MovementDialog.jsx
@@ -1,13 +1,25 @@
+import { useMemo } from "react";
 import { useTranslation } from "react-i18next";
 import { Controller } from "react-hook-form";
 
 // components
 import FormDialog from "../../../components/Dialog/FormDialog";
 import TextInput from "../../../components/Form/TextInput";
+import SelectInput from "../../../components/Form/SelectInput";
+
+// types
+import { types } from "../../../db/types/products";
 
 export function MovementForm(props) {
-  const { control } = props;
+  const { control, isLoading } = props;
   const { t } = useTranslation();
+
+  const typeOptions = useMemo(() => {
+    return [
+      { id: 0, value: t("_pages:movements.inputs.type.name") },
+      ...(types?.data ?? []),
+    ];
+  }, []);
 
   return (
     <div className="flex flex-col gap-5">
@@ -17,12 +29,31 @@ export function MovementForm(props) {
           required: t("_pages:movements.inputs.name.required"),
         }}
         name="name"
+        disabled={isLoading}
         render={({ field }) => (
           <TextInput
             required
             maxLength={20}
             placeholder={t("_pages:movements.inputs.name.name")}
             {...field}
+          />
+        )}
+      />
+      <Controller
+        control={control}
+        rules={{
+          invalid: t("_pages:movements.inputs.type.invalid"),
+        }}
+        name="type"
+        disabled={isLoading}
+        render={({ field: { value, onChange, ...rest } }) => (
+          <SelectInput
+            required
+            options={typeOptions}
+            value={value}
+            onChange={(e) => onChange(e.target.value)}
+            placeholder={t("_pages:movements.inputs.type.name")}
+            {...rest}
           />
         )}
       />
