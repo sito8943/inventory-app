d7997ba873374c63668bc6f3683dbec1a7bdcee6
Sito <sito8943@gmail.com>
2025-04-08 08:34:12 -0400

    removed log

--- src/hooks/useFormDialog.jsx
+++ src/hooks/useFormDialog.jsx
@@ -64,7 +64,6 @@ function useFormDialog(props) {
 
   const releaseFormError = useCallback(() => {
     const inputs = document.querySelectorAll("input, textarea, select");
-    console.log(inputs);
     inputs.forEach((input) => {
       input.classList.remove("error");
     });
