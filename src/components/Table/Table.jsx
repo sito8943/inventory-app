a8953875941a55d736eb34e25e9ac89d339c0000
Sito <sito8943@gmail.com>
2025-04-09 11:47:23 -0400

    added table

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -1,7 +1,38 @@
-import React from "react";
+function Table(props) {
+  const { columns, rows } = props;
 
-function Table() {
-  return <div></div>;
+  return (
+    <table className="">
+      <thead>
+        <tr>
+          {columns.map((column) => (
+            <th
+              key={column.id}
+              className={`border-border border-2 text-gray-300 px-5 py-2 text-base ${column.className}`}
+            >
+              {column.label}
+            </th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {rows.map((row) => (
+          <tr key={row.id?.value} className={`${row.className ?? ""}`}>
+            {columns.map((column) => (
+              <td
+                className={`border-border border-2 rounded-2xl px-5 py-2 text-gray-50 text-sm ${
+                  row[column.id]?.className ?? ""
+                }`}
+                key={column.id}
+              >
+                {row[column.id]?.value ?? ""}
+              </td>
+            ))}
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
 }
 
 export default Table;
---
+++ src/components/Table/useTableOptions.jsx
@@ -0,0 +1,54 @@
+import { useMemo } from "react";
+import { useTranslation } from "react-i18next";
+
+function useTableOptions(props) {
+  const { t } = useTranslation();
+
+  const { data, entity, ignoreColumns = [], customOptions = {} } = props;
+
+  const rows = useMemo(() => {
+    if (!data) return [];
+
+    return data.map((item) => {
+      const row = { ...item };
+      ignoreColumns.forEach((column) =>
+        column !== "id" ? delete row[column] : null
+      );
+
+      Object.keys(row).forEach((column) => {
+        const newRow = {};
+        if (customOptions[column]?.rowOptions?.parser)
+          newRow.value = customOptions[column].rowOptions.parser(row[column]);
+        else newRow.value = row[column];
+
+        if (customOptions[column]?.rowOptions?.className)
+          newRow.className = customOptions[column].rowOptions.className;
+
+        row[column] = newRow;
+      });
+
+      return row;
+    });
+  }, [data, ignoreColumns]);
+
+  const columns = useMemo(() => {
+    if (!data || data.length === 0) return [];
+
+    const firstRow = data[0];
+    const keys = Object.keys(firstRow).filter(
+      (key) => !ignoreColumns.includes(key)
+    );
+
+    return keys.map((key) => ({
+      id: key,
+      label: t(`_pages:${entity}.properties.${key}`),
+      className: customOptions[key]?.columnOptions?.className
+        ? customOptions[key]?.columnOptions?.className
+        : "text-left",
+    }));
+  }, [data, ignoreColumns]);
+
+  return { rows, columns };
+}
+
+export default useTableOptions;
