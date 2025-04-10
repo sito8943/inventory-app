fd2df49b4a52ed15acd57c0c5b6ad2cbcc06999b
Sito <sito8943@gmail.com>
2025-04-10 07:35:17 -0400

    format hoooks imports

---
+++ src/hooks/index.js
@@ -0,0 +1,6 @@
+import useConfirmationForm from "./useConfirmationForm";
+import useDialog from "./useDialog";
+import useFormDialog from "./useFormDialog";
+import useTimeAge from "./useTimeAge";
+
+export { useConfirmationForm, useDialog, useFormDialog, useTimeAge };
