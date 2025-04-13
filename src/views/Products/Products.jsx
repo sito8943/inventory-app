0fe8bd593334b0dd5255aae943bc9ac487ffd420
Sito <sito8943@gmail.com>
2025-04-12 21:04:10 -0400

    reduced test error

--- src/views/Products/Products.jsx
+++ src/views/Products/Products.jsx
@@ -69,7 +69,6 @@ function Products() {
 
   return (
     <main className="p-5">
-      {a[5]}
       <div className="apparition flex flex-col gap-5">
         <h2 className="text-xl">{t("_pages:products.title")}</h2>
         {isLoading ? (
