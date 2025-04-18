085f691cbf2b0145215a1d5fa9eac4a9ba02b46e
Sito8943 <sito8943@gmail.com>
2025-04-18 11:57:10 -0400

    pretty product card

--- src/components/Chip/Chip.jsx
+++ src/components/Chip/Chip.jsx
@@ -1,12 +1,19 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 /**
  * Chip component
  * @param {object} props - componente props
  * @returns Chip component
  */
 const Chip = (props) => {
-  const { text, children, variant = "primary", className, ...rest } = props;
+  const { text, children, icon, variant = "primary", className, ...rest } = props;
 
-  return <div className={`chip ${variant} ${className}`} {...rest}>{text ?? children}</div>;
+  return (
+    <div className={`chip ${variant} ${className}`} {...rest}>
+      {icon ? <FontAwesomeIcon icon={icon} /> : null}
+      {text ?? children}
+    </div>
+  );
 };
 
-export default Chip
+export default Chip;
--- src/views/Products/components/ProductCard.jsx
+++ src/views/Products/components/ProductCard.jsx
@@ -8,86 +8,87 @@ import { css } from "@emotion/css";
 import { Chip, Actions } from "../../../components";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-    faBoxArchive,
-    faDollar,
-    faCoins,
+  faBoxArchive,
+  faDollar,
+  faCoins,
 } from "@fortawesome/free-solid-svg-icons";
 
 function ProductCard(props) {
-    const { t } = useTranslation();
+  const { t } = useTranslation();
 
-    const {
-        id,
-        onClick,
-        actions,
-        name,
-        description,
-        color,
-        categoryName,
-        price,
-        cost,
-        stock,
-    } = props;
+  const {
+    id,
+    onClick,
+    actions,
+    name,
+    description,
+    color,
+    categoryName,
+    price,
+    cost,
+    stock,
+  } = props;
 
-    const styles = css({
-        borderLeftColor: color,
-        borderLeftWidth: "4px",
-        borderLeftStyle: "solid",
-    });
+  const styles = css({
+    borderLeftColor: color,
+    borderLeftWidth: "4px",
+    borderLeftStyle: "solid",
+  });
 
-    return (
-        <div
-            className={`${styles} flex flex-col justify-between items-start min-h-40 w-60 max-xs:w-full rounded-2xl p-3 group border-primary/30 hover:!border-primary border-2 animated`}
-        >
-            <button
-                className="cursor-pointer h-full w-full flex flex-col gap-1"
-                name={t("_pages:products.forms.edit")}
-                aria-label={t("_pages:products.forms.editAria")}
-                onClick={() => onClick(id)}
-            >
-                <div className="flex justify-between items-start">
-                    <h3 className="text-lg text-start font-semibold !text-gray-200">
-                        {name}
-                    </h3>
-                </div>
-                <p className="text-sm text-gray-500 text-start">
-                    {description ?? t("_pages:products.inputs.description.empty")}
-                </p>
-
-                <Chip variant="none" className="!p-0">
-                    <FontAwesomeIcon icon={faDollar} />
-                    <p className="font-medium">
-                        {t("_pages:products.inputs.price.name")}:
-                    </p>
-                    <p className={!price ? "italic" : ""}>
-                        {price ? `$${price}` : t("_pages:products.inputs.price.empty")}
-                    </p>
-                </Chip>
-                <Chip variant="none" className="!p-0">
-                    <FontAwesomeIcon icon={faCoins} />{" "}
-                    <p className="font-medium">
-                        {t("_pages:products.inputs.cost.name")}:
-                    </p>
-                    <p className={!cost ? "italic" : ""}>
-                        {cost ? `${cost}` : t("_pages:products.inputs.cost.empty")}
-                    </p>
-                </Chip>
-                <Chip variant="none" className="!p-0">
-                    <FontAwesomeIcon icon={faBoxArchive} />{" "}
-                    <p className="font-medium">
-                        {t("_pages:products.inputs.stock.name")}:
-                    </p>
-                    <p className={!stock ? "italic" : ""}>
-                        {stock
-                            ? `${stock} ${t("_pages:products.inputs.stock.units")}`
-                            : t("_pages:products.inputs.stock.empty")}
-                    </p>
-                </Chip>
-            </button>
-
-            <Actions actions={actions} />
+  return (
+    <div
+      className={`${styles} flex flex-col justify-between items-start h-50 w-60 max-xs:w-full rounded-2xl p-3 pb-1 group border-primary/30 hover:!border-primary border-2 animated`}
+    >
+      <button
+        className="cursor-pointer h-full w-full flex flex-col gap-1"
+        name={t("_pages:products.forms.edit")}
+        aria-label={t("_pages:products.forms.editAria")}
+        onClick={() => onClick(id)}
+      >
+        <div className="flex justify-between items-start">
+          <h3 className="text-lg text-start font-semibold !text-gray-200">
+            {name}
+          </h3>
         </div>
-    );
+        <p className="text-sm text-gray-500 text-start">
+          {description ?? t("_pages:products.inputs.description.empty")}
+        </p>
+
+        <Chip icon={faDollar} variant="none" className="!p-0">
+          <p className="font-medium">
+            {t("_pages:products.inputs.price.name")}:
+          </p>
+          <p className={!price ? "italic" : ""}>
+            {price ? `$${price}` : t("_pages:products.inputs.price.empty")}
+          </p>
+        </Chip>
+        <Chip icon={faCoins} variant="none" className="!p-0">
+          <p className="font-medium">
+            {t("_pages:products.inputs.cost.name")}:
+          </p>
+          <p className={!cost ? "italic" : ""}>
+            {cost ? `${cost}` : t("_pages:products.inputs.cost.empty")}
+          </p>
+        </Chip>
+        <Chip icon={faBoxArchive} variant="none" className="!p-0">
+          <p className={stock ? "font-medium" : "italic"}>
+            {stock ? (
+              <>
+                {t("_pages:products.inputs.stock.name")}:{" "}
+                <span>
+                  {`${stock} ${t("_pages:products.inputs.stock.units")}`}
+                </span>
+              </>
+            ) : (
+              t("_pages:products.inputs.stock.empty")
+            )}
+          </p>
+        </Chip>
+      </button>
+
+      <Actions actions={actions} />
+    </div>
+  );
 }
 
 export default ProductCard;
