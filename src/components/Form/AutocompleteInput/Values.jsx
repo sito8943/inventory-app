4e58d84598a3cff3013ebd2a5650d78ed2d2e9fe
Sito <sito8943@gmail.com>
2025-04-29 08:58:30 -0400

    better margin on autocomplete values

--- src/components/Form/AutocompleteInput/Values.jsx
+++ src/components/Form/AutocompleteInput/Values.jsx
@@ -4,7 +4,7 @@ export const Values = (props) => {
   const { list = [], onDelete } = props;
 
   return (
-    <div className="flex items-center justify-start flex-wrap my-4 gap-2">
+    <div className="flex items-center justify-start flex-wrap mt-4 gap-2">
       {list.map((selected, i) => (
         <DeletableChip
           key={selected.value}
