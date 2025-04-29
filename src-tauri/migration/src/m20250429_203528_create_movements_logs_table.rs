67d768031aeadb394d73858f3d109904188f2fc6
Sito <sito8943@gmail.com>
2025-04-29 16:38:04 -0400

    added migration for movement logs

---
+++ src-tauri/migration/src/m20250429_203528_create_movements_logs_table.rs
@@ -0,0 +1,51 @@
+use sea_orm_migration::{prelude::*, schema::*};
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .create_table(
+                Table::create()
+                    .table(MovementLogs::Table)
+                    .if_not_exists()
+                    .col(pk_auto(MovementLogs::Id))
+                    .col(ColumnDef::new(MovementLogs::Movement).integer().not_null())
+                    .col(ColumnDef::new(MovementLogs::Product).integer().not_null())
+                    .col(ColumnDef::new(MovementLogs::Count).integer().not_null())
+                    .col(
+                        ColumnDef::new(MovementLogs::CreatedAt)
+                            .timestamp()
+                            .default(SimpleExpr::Keyword(Keyword::CurrentTimestamp)),
+                    )
+                    .col(
+                        ColumnDef::new(MovementLogs::UpdatedAt)
+                            .timestamp()
+                            .default(SimpleExpr::Keyword(Keyword::CurrentTimestamp)),
+                    )
+                    .col(ColumnDef::new(MovementLogs::DeletedAt).timestamp())
+                    .to_owned(),
+            )
+            .await
+    }
+
+    async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .drop_table(Table::drop().table(MovementLogs::Table).to_owned())
+            .await
+    }
+}
+
+#[derive(DeriveIden)]
+enum MovementLogs {
+    Table,
+    Id,
+    Movement,
+    Product,
+    Count,
+    CreatedAt,
+    UpdatedAt,
+    DeletedAt,
+}
