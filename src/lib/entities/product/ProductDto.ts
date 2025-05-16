09d4fd1481fe3040f7618787a2ed46be4b1180a4
Sito <sito8943@gmail.com>
2025-05-16 08:49:57 -0400

    fixed: description goes on product dto

--- src/lib/entities/product/ProductDto.ts
+++ src/lib/entities/product/ProductDto.ts
@@ -1,6 +1,7 @@
 import { BaseEntityDto, CommonCategoryDto, CommonProductDto } from "lib";
 
 export interface ProductDto extends CommonProductDto, BaseEntityDto {
+  description: string;
   price: number;
   cost: number;
   stock: number;
