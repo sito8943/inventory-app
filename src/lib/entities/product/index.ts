adc8e77de4b379393eca22b1cf1eb8fea8991811
Sito <sito8943@gmail.com>
2025-05-21 14:19:07 -0400

    fixed home dto

--- src/lib/entities/product/index.ts
+++ src/lib/entities/product/index.ts
@@ -3,6 +3,7 @@ import { CommonProductDto } from "./CommonProductDto.ts";
 import { FilterProductDto } from "./FilterProductDto.ts";
 import { ProductDto } from "./ProductDto.ts";
 import { UpdateProductDto } from "./UpdateProductDto.ts";
+import { CategoryProductDto } from "./CategoryProductDto.ts";
 
 export type {
   AddProductDto,
@@ -10,4 +11,5 @@ export type {
   FilterProductDto,
   ProductDto,
   UpdateProductDto,
+  CategoryProductDto,
 };
