f9432bd7a9b6c72935b2c53713e49ffcf67af39d
Sito <sito8943@gmail.com>
2025-05-21 09:29:37 -0400

    fixed init for movements

--- src/providers/ManagerProvider.tsx
+++ src/providers/ManagerProvider.tsx
@@ -1,9 +1,13 @@
 import { createContext, useContext, useEffect } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useTranslation } from "react-i18next";
 
 // manager
 import Manager from "../db/Manager";
 import { ManagerProviderContextType, BasicProviderPropTypes } from "./types.ts";
+
+// hooks
+import { defaultMovements } from "hooks";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -28,10 +32,13 @@ const ManagerContext = createContext({} as ManagerProviderContextType);
 const ManagerProvider = (props: BasicProviderPropTypes) => {
   const { children } = props;
 
+  const { i18n } = useTranslation();
+
   const manager = new Manager();
 
   useEffect(() => {
-    manager.Movements.init().then(() => console.info("init"));
+    const defaultValues = defaultMovements[i18n.language];
+    manager.Movements.init(defaultValues).then(() => console.info("init"));
   }, []);
 
   return (
