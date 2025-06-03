5d482fa4a3ebe12d49fda8b504f9ac72a6cf42d4
Sito <sito8943@gmail.com>
2025-06-03 08:52:09 -0400

    network provider

--- src/providers/ConfigProvider.tsx
+++ src/providers/ConfigProvider.tsx
@@ -1,84 +1,62 @@
 import {
-  createContext,
-  useCallback,
-  useContext,
-  useEffect,
-  useMemo,
-  useState,
+    createContext,
+    useCallback,
+    useContext,
+    useEffect,
+    useMemo,
+    useState,
 } from "react";
 
 // config
-import { config } from "../config.ts";
+import {config} from "../config.ts";
 
 // types
 import {
-  BasicProviderPropTypes,
-  ConfigProviderContextType,
-  FileDataType,
+    BasicProviderPropTypes,
+    ConfigProviderContextType,
+    FileDataType,
 } from "./types.ts";
 
 // client
 import TauriClient from "../db/TauriClient.ts";
-import { QueryKey, useQuery } from "@tanstack/react-query";
 
 const ConfigContext = createContext({} as ConfigProviderContextType);
 
 const ConfigProvider = (props: BasicProviderPropTypes) => {
-  const { children } = props;
+    const {children} = props;
 
-  const tauriClient = useMemo(() => new TauriClient(), []);
-  const [data, setData] = useState<FileDataType>();
-  const [connected, setConnected] = useState(false);
+    const tauriClient = useMemo(() => new TauriClient(), []);
+    const [data, setData] = useState<FileDataType>();
 
-  const init = useCallback(() => {
-    try {
-      const file = tauriClient.readFile(config.configFile);
-    } catch (e) {
-      console.error((e as Error).message);
-    }
-  }, [tauriClient]);
+    const init = useCallback(() => {
+        try {
+            const file = tauriClient.readFile(config.configFile);
+        } catch (e) {
+            console.error((e as Error).message);
+        }
+    }, [tauriClient]);
 
-  const checkConnection = useCallback(async () => {
-    try {
-      const response = await fetch(`${config.apiUrl}app/ping`, {
-        method: "GET",
-        cache: "no-cache",
-      });
-      setConnected(true);
-      return response.ok; // true si status 2xx
-    } catch (error) {
-      setConnected(false);
-      console.error(error);
-      return false;
-    }
-  }, []);
 
-  const pingServer = useQuery({
-    initialData: true,
-    queryKey: ["ping"],
-    queryFn: checkConnection,
-  });
+    useEffect(() => {
+        init();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
 
-  useEffect(() => {
-    init();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
-  return (
-    <ConfigContext.Provider
-      value={{ data, connected, pingServer, updateData: setData }}
-    >
-      {children}
-    </ConfigContext.Provider>
-  );
+    return (
+        <ConfigContext.Provider
+            value={{data, updateData: setData}}
+        >
+            {children}
+        </ConfigContext.Provider>
+    );
 };
 
 const useConfig = () => {
-  const context = useContext(ConfigContext);
+    const context = useContext(ConfigContext);
 
-  if (context === undefined)
-    throw new Error("configContext must be used within a Provider");
-  return context;
+    if (context === undefined)
+        throw new Error("configContext must be used within a Provider");
+    return context;
 };
 
-export { ConfigProvider, useConfig };
+export {ConfigProvider, useConfig};
