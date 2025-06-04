9906f6ea530bd54ceb293bd098b1c022fe326ba2
Sito <sito8943@gmail.com>
2025-06-04 07:20:08 -0400

    better error

--- src/db/APIClient.ts
+++ src/db/APIClient.ts
@@ -1,95 +1,94 @@
 // services
-import { buildQueryUrl, makeRequest } from "./utils/services";
+import {buildQueryUrl, makeRequest} from "./utils/services";
 
 // types
-import { QueryResult } from "lib";
+import {QueryResult} from "lib";
 
 /**
  * @class APIClient
  * @description it has all base methods
  */
 export class APIClient {
-  async doQuery<TResponse, TBody = unknown>(
-    endpoint: string,
-    method = "GET",
-    query?: any,
-    body?: TBody,
-  ) {
-    const builtUrl = buildQueryUrl(endpoint, query);
-    const { data: result, error } = await makeRequest(builtUrl, method, body);
-    if (error) throw new Error(error.message);
+    async doQuery<TResponse, TBody = unknown>(
+        endpoint: string,
+        method = "GET",
+        query?: any,
+        body?: TBody,
+    ) {
+        const builtUrl = buildQueryUrl(endpoint, query);
+        const {data: result, error} = await makeRequest(builtUrl, method, body);
+        if (error) throw new Error(error.message);
 
-    return result as TResponse;
-  }
+        return result as TResponse;
+    }
 
-  /**
-   * @description Get all objects
-   * @param endpoint - backed endpoint
-   * @param query - query parameters
-   * @returns Result list
-   */
-  async get<TDto, TFilter>(endpoint: string, query?: TFilter) {
-    const builtUrl = buildQueryUrl<TFilter>(endpoint, query);
-    const { data: result, error } = await makeRequest(builtUrl, "GET", null);
+    /**
+     * @description Get all objects
+     * @param endpoint - backed endpoint
+     * @param query - query parameters
+     * @returns Result list
+     */
+    async get<TDto, TFilter>(endpoint: string, query?: TFilter) {
+        const builtUrl = buildQueryUrl<TFilter>(endpoint, query);
+        const {data: result, error} = await makeRequest(builtUrl, "GET", null);
+        if (error) throw new Error(`${error.status} ${error.message}`);
 
-    if (error) throw new Error(error.message);
+        return result as QueryResult<TDto>;
+    }
 
-    return result as QueryResult<TDto>;
-  }
+    /**
+     * @description Get entity by id
+     * @param endpoint - backed endpoint
+     * @param data - data to update
+     * @returns updated entity
+     */
+    async patch<TDto, TUpdateDto>(
+        endpoint: string,
+        data: TUpdateDto,
+    ): Promise<TDto> {
+        const {error, data: result} = await makeRequest<TUpdateDto, TDto>(
+            endpoint,
+            "PATCH",
+            data,
+        );
 
-  /**
-   * @description Get entity by id
-   * @param endpoint - backed endpoint
-   * @param data - data to update
-   * @returns updated entity
-   */
-  async patch<TDto, TUpdateDto>(
-    endpoint: string,
-    data: TUpdateDto,
-  ): Promise<TDto> {
-    const { error, data: result } = await makeRequest<TUpdateDto, TDto>(
-      endpoint,
-      "PATCH",
-      data,
-    );
+        if (error) throw new Error(error.message);
 
-    if (error) throw new Error(error.message);
+        return result;
+    }
 
-    return result;
-  }
+    /**
+     * @param endpoint - backend endpoint
+     * @param  data - value to insert
+     * @returns delete result
+     */
+    async delete(endpoint: string, data: any) {
+        const {error, data: result} = await makeRequest<number[], number>(
+            endpoint,
+            "DELETE",
+            data,
+        );
 
-  /**
-   * @param endpoint - backend endpoint
-   * @param  data - value to insert
-   * @returns delete result
-   */
-  async delete(endpoint: string, data: any) {
-    const { error, data: result } = await makeRequest<number[], number>(
-      endpoint,
-      "DELETE",
-      data,
-    );
+        if (error) throw new Error(error.message);
 
-    if (error) throw new Error(error.message);
+        return result;
+    }
 
-    return result;
-  }
+    /**
+     *
+     * @param endpoint - backend endpoint
+     * @param  data - value to insert
+     * @returns inserted item
+     */
+    async post<TDto, TAddDto>(endpoint: string, data: TAddDto): Promise<TDto> {
+        const {error, data: result} = await makeRequest<TAddDto, TDto>(
+            endpoint,
+            "POST",
+            data,
+        );
 
-  /**
-   *
-   * @param endpoint - backend endpoint
-   * @param  data - value to insert
-   * @returns inserted item
-   */
-  async post<TDto, TAddDto>(endpoint: string, data: TAddDto): Promise<TDto> {
-    const { error, data: result } = await makeRequest<TAddDto, TDto>(
-      endpoint,
-      "POST",
-      data,
-    );
+        if (error) throw new Error(error.message);
 
-    if (error) throw new Error(error.message);
-
-    return result;
-  }
+        return result;
+    }
 }
--- src/views/Products/Products.tsx
+++ src/views/Products/Products.tsx
@@ -1,136 +1,136 @@
-import { useCallback, useMemo } from "react";
-import { useTranslation } from "react-i18next";
+import {useCallback, useMemo} from "react";
+import {useTranslation} from "react-i18next";
 
 // providers
-import { useManager } from "providers";
+import {useManager} from "providers";
 
 // components
-import { ConfirmationDialog, Page, PrettyGrid } from "../../components";
+import {ConfirmationDialog, Page, PrettyGrid} from "../../components";
 import {
-  AddProductDialog,
-  DoMovementDialog,
-  EditProductDialog,
-  MovementLogsDialog,
-  ProductCard,
+    AddProductDialog,
+    DoMovementDialog,
+    EditProductDialog,
+    MovementLogsDialog,
+    ProductCard,
 } from "./components";
-import { Error } from "components";
-import { TabsLayout } from "../../components/TabsLayout";
+import {Error} from "components";
+import {TabsLayout} from "../../components/TabsLayout";
 
 // hooks
 import {
-  useAddProduct,
-  useDoMovement,
-  useEditProduct,
-  useMovementLogs,
+    useAddProduct,
+    useDoMovement,
+    useEditProduct,
+    useMovementLogs,
 } from "./hooks/dialogs/";
 import {
-  useDeleteDialog,
-  useProductsList,
-  ProductsQueryKeys,
-  useCategoriesCommon,
+    useDeleteDialog,
+    useProductsList,
+    ProductsQueryKeys,
+    useCategoriesCommon,
 } from "hooks";
 
 // types
-import { ProductDto } from "lib";
+import {ProductDto} from "lib";
 
 function Products() {
-  const { t } = useTranslation();
+    const {t} = useTranslation();
 
-  const manager = useManager();
+    const manager = useManager();
 
-  const productQuery = useProductsList({});
+    const productQuery = useProductsList({});
 
-  const categoryQuery = useCategoriesCommon();
+    const categoryQuery = useCategoriesCommon();
 
-  const isLoading = useMemo(
-    () => productQuery.isLoading && categoryQuery.isLoading,
-    [productQuery.isLoading, categoryQuery.isLoading],
-  );
+    const isLoading = useMemo(
+        () => productQuery.isLoading && categoryQuery.isLoading,
+        [productQuery.isLoading, categoryQuery.isLoading],
+    );
 
-  const error = useMemo(
-    () => productQuery.error || categoryQuery.error,
-    [productQuery.error, categoryQuery.error],
-  );
+    const error = useMemo(
+        () => productQuery.error || categoryQuery.error,
+        [productQuery.error, categoryQuery.error],
+    );
 
-  // #region actions
+    // #region actions
 
-  const deleteProduct = useDeleteDialog({
-    mutationFn: (data: number[]) => manager.Products.softDelete(data),
-    ...ProductsQueryKeys.all(),
-  });
+    const deleteProduct = useDeleteDialog({
+        mutationFn: (data: number[]) => manager.Products.softDelete(data),
+        ...ProductsQueryKeys.all(),
+    });
 
-  const addProduct = useAddProduct();
+    const addProduct = useAddProduct();
 
-  const editProduct = useEditProduct();
+    const editProduct = useEditProduct();
 
-  const doMovement = useDoMovement();
+    const doMovement = useDoMovement();
 
-  const movementLogs = useMovementLogs();
+    const movementLogs = useMovementLogs();
 
-  // #endregion
+    // #endregion
 
-  const getActions = useCallback(
-    (record: ProductDto) => [
-      doMovement.action(record),
-      movementLogs.action(record),
-      deleteProduct.action(record),
-    ],
-    [doMovement, movementLogs, deleteProduct],
-  );
+    const getActions = useCallback(
+        (record: ProductDto) => [
+            doMovement.action(record),
+            movementLogs.action(record),
+            deleteProduct.action(record),
+        ],
+        [doMovement, movementLogs, deleteProduct],
+    );
 
-  const tabs = useMemo(
-    () =>
-      categoryQuery.data?.map(({ id, name }) => ({
-        id,
-        label: name,
-        content: (
-          <div id={name} key={id} className="p-5">
-            <PrettyGrid
-              data={productQuery?.data ? productQuery?.data[id] : []}
-              emptyMessage={t("_pages:products.empty")}
-              renderComponent={(product) => (
-                <ProductCard
-                  actions={getActions(product)}
-                  onClick={(id: number) => editProduct.onClick(id)}
-                  {...product}
-                />
-              )}
-            />
-          </div>
-        ),
-      })) ?? [],
-    [categoryQuery.data, productQuery?.data, t, getActions, editProduct],
-  );
+    const tabs = useMemo(
+        () =>
+            categoryQuery.data?.map(({id, name}) => ({
+                id,
+                label: name,
+                content: (
+                    <div id={name} key={id} className="p-5">
+                        <PrettyGrid
+                            data={productQuery?.data ? productQuery?.data[id] : []}
+                            emptyMessage={t("_pages:products.empty")}
+                            renderComponent={(product) => (
+                                <ProductCard
+                                    actions={getActions(product)}
+                                    onClick={(id: number) => editProduct.onClick(id)}
+                                    {...product}
+                                />
+                            )}
+                        />
+                    </div>
+                ),
+            })) ?? [],
+        [categoryQuery.data, productQuery?.data, t, getActions, editProduct],
+    );
 
-  return (
-    <Page
-      title={t("_pages:products.title")}
-      isLoading={isLoading}
-      addOptions={{
-        onClick: () => addProduct.onClick(),
-        disabled: isLoading,
-        tooltip: t("_pages:products.add"),
-      }}
-    >
-      {!error ? (
-        <>
-          <TabsLayout
-            defaultTab={tabs[0]?.id ?? 0}
-            tabs={tabs}
-            className="h-full"
-          />
-          {/* Dialogs */}
-          <AddProductDialog {...addProduct} />
-          <EditProductDialog {...editProduct} />
-          <DoMovementDialog {...doMovement} />
-          <MovementLogsDialog {...movementLogs} />
-          <ConfirmationDialog {...deleteProduct} />
-        </>
-      ) : (
-        <Error message={error?.message} />
-      )}
-    </Page>
-  );
+    return (
+        <Page
+            title={t("_pages:products.title")}
+            isLoading={isLoading}
+            addOptions={{
+                onClick: () => addProduct.onClick(),
+                disabled: isLoading,
+                tooltip: t("_pages:products.add"),
+            }}
+        >
+            {!error ? (
+                <>
+                    <TabsLayout
+                        defaultTab={tabs[0]?.id ?? 0}
+                        tabs={tabs}
+                        className="h-full"
+                    />
+                    {/* Dialogs */}
+                    <AddProductDialog {...addProduct} />
+                    <EditProductDialog {...editProduct} />
+                    <DoMovementDialog {...doMovement} />
+                    <MovementLogsDialog {...movementLogs} />
+                    <ConfirmationDialog {...deleteProduct} />
+                </>
+            ) : (
+                <Error message={error?.message}/>
+            )}
+        </Page>
+    );
 }
 
 export default Products;
