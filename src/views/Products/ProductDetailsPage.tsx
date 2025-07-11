b29c672ba41f8ec315821f20c22adbef66d8284f
Sito <sito8943@gmail.com>
2025-07-11 18:02:11 -0400

    working on ProductDetails

--- src/views/Products/ProductDetailsPage.tsx
+++ src/views/Products/ProductDetailsPage.tsx
@@ -1,3 +1,23 @@
+import { useTranslation } from "react-i18next";
+import { useParams } from "react-router-dom";
+
+// components
+import { Page } from "components";
+import { ProductForm } from "./components";
+
+// hooks
+import { useProductForm } from "./hooks";
+
 export function ProductDetailsPage() {
-  return <div></div>;
+  const { id = 0 } = useParams();
+
+  const { t } = useTranslation();
+
+  const productForm = useProductForm({ id: Number(id) });
+
+  return (
+    <Page title={t(`_pages:products.title`)} isLoading={productForm.isLoading}>
+      <ProductForm {...productForm} />
+    </Page>
+  );
 }
