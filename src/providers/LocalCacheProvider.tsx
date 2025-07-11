6be767b5caa5dc9a8958ebfd4090e937905ee703
Sito <sito8943@gmail.com>
2025-07-11 19:48:21 -0400

    local cache removed unused import

--- src/providers/LocalCacheProvider.tsx
+++ src/providers/LocalCacheProvider.tsx
@@ -1,11 +1,5 @@
 /* eslint-disable react-refresh/only-export-components */
-import {
-  createContext,
-  useCallback,
-  useContext,
-  useMemo,
-  useState,
-} from "react";
+import { createContext, useCallback, useContext, useState } from "react";
 
 // config
 import { config } from "../config.ts";
