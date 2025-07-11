317a3e274db5e702b7736bf1ab7c8bce7d9e3ae1
Sito <sito8943@gmail.com>
2025-07-11 15:59:15 -0400

    working on input styles

--- src/components/Form/ParagraphInput.tsx
+++ src/components/Form/ParagraphInput.tsx
@@ -40,13 +40,13 @@ export const ParagraphInput = forwardRef(function (
 
   return (
     <div
-      className={`relative z-0 w-full mb-5 group h-80 ${containerClassName}`}
+      className={`relative z-0 w-full mb-5 group ${containerClassName}`}
     >
       <textarea
         ref={ref}
         name={name}
         id={id}
-        className={`block py-2.5 px-0 w-full text-sm bg-transparent border-0 border-b-2 appearance-none focus:outline-none focus:ring-0 ${inputStateClassName(state)} ${inputClassName} peer`}
+        className={`text-input text-area ${inputStateClassName(state)} ${inputClassName} peer`}
         placeholder=""
         required={required}
         value={value}
@@ -56,13 +56,13 @@ export const ParagraphInput = forwardRef(function (
       ></textarea>
       <label
         htmlFor={name}
-        className={`peer-focus:font-medium absolute text-sm duration-300 transform -translate-y-7 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-7 ${labelStateClassName(state)} ${labelClassName}`}
+        className={`text-input-label ${labelStateClassName(state)} ${labelClassName}`}
       >
         {label}
         {required ? " *" : ""}
       </label>
       <p
-        className={`mt-2 text-sm ${helperTextStateClassName(state)} ${helperTextClassName}`}
+        className={`text-input-helper-text ${helperTextStateClassName(state)} ${helperTextClassName}`}
       >
         {state !== "error" && state !== "good" ? placeholder : helperText}
       </p>
